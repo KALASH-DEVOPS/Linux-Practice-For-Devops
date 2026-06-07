264f421b66ab07cad798280cc11084b3c66631b1
Ubuntu <ubuntu@ip-172-31-44-15.eu-north-1.compute.internal>
2026-06-07 22:04:17 +0000

    added linux day02 practice script

---
+++ linuxday02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "==File permissions=="
+ls -l
+chmod 755 linuxday02.sh
+ls -l
+
+
+echo "==Present working Directory==="
+
+pwd
+
+
+echo "==Change directory=="
+
+mkdir kalash
+ls
